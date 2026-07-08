82917db4ac2e7fadf845997a9ea218c888a783fa
luismartin023 <mkzcompanyofficial@gmail.com>
2026-07-08 00:26:57 -0400

    refactor: limpiar rama yadfreidel - solo Controllers

--- SistemaArquiler-MVC/SISTEMA/Interfaces/IApartamentoRepository.cs
+++
@@ -1,9 +0,0 @@
-using SistemaGestionResidencial.Models;
-
-namespace SistemaGestionResidencial.Interfaces
-{
-    public interface IApartamentoRepository : IRepository<Apartamento>
-    {
-        IEnumerable<Apartamento> ObtenerPorEstado(EstadoApartamento estado);
-    }
-}
